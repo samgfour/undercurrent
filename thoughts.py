d00c4810c344195e1ebb7202aadf29112b221b19
samgfour <wangusam01@gmail.com>
2026-09-24 10:17:32 +0300

    idea commit

---
+++ thoughts.py
@@ -0,0 +1,11 @@
+# record thoughts and assign labels for future reference
+#analyze relationships over time
+
+# what are you thinking about?
+#what feeling is it related to?
+#do you like the thought?
+#would you like to keep it?
+# save thoughts and their labels/feelings in a csv
+#search through past thoughts
+#exit program or app
+
